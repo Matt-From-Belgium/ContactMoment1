f86cf6b316463369bae1039fef11caff70f338c3
Matthias Bauw <matthias.bauw@student.vives.be>
2020-01-24 16:16:02 +0100

    Add project files.

---
+++ ContactMoment1/Controllers/StudentController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContactMoment1.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactMoment1.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Lijst()
+        {
+            return View();
+        }
+        public IActionResult zonderLayout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index (PersonVM personVM)
+        {
+            string naam = personVM.Naam;
+            return View();
+        }
+    }
+}
---
+++ ContactMoment1/ViewModels/PersonVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactMoment1.ViewModels
+{
+    public class PersonVM
+    {
+        public String Naam { get; set; }
+
+        public String Voornaam { get; set; }
+    }
+}
